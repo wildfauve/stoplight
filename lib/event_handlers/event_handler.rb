f2c8885fbea9f8a36ee7808d31f62803a9a5c763
Col Perks <wild.fauve@gmail.com>
2016-08-17 16:48:25 +1200

    fix problem

--- lib/event_handlers/event_handler.rb
+++
@@ -1,9 +0,0 @@
-class EventHandler
-
-  include AutoInject["event_handlers.event_handler_factory", "subscriber"]
-
-  def call(event)
-    event_handler_factory.(event[:kind]).(event, subscriber.new.find_by_party(event))
-  end
-
-end
--- lib/values/subscriber_value.rb
+++
@@ -1,9 +0,0 @@
-require './lib/types'
-
-class SubscriberValue < Dry::Types::Struct
-
-  attribute :name, Types::String
-  attribute :email, Types::String
-  attribute :subjects, Types::Hash
-
-end
